4ca2877588a2e0b5e7cc2ca82f9968d3263633fd
Diego Vasquez <davasqueza@unal.edu.co>
2023-10-02 14:04:33 -0500

    Implemented ASG Policy

--- ASG Fundamentals/ec2-user-data.sh
+++ ASG Fundamentals/ec2-user-data.sh
@@ -3,6 +3,11 @@
 # install httpd (Linux 2 version)
 yum update -y
 yum install -y httpd # Installs Apache HTTP Server
+
+amazon-linux-extras install epel -y
+yum install stress -y  # Installs stress utility
+# stress -c 4 -t 500 # Stress the instance to test ASG
+
 systemctl start httpd
 systemctl enable httpd
 echo "<h1>Hello World from $(hostname -f)</h1> using CloudFormation" > /var/www/html/index.html
